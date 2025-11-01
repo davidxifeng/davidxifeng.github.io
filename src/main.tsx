0f6a2323719fec1468ef78bae635cec6b0f86c6a
David Feng <davidxifeng@gmail.com>
2025-11-01 13:57:55 +0900

    remove cf worker project

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen.ts';
 
-// Import Query Client
-import { queryClient } from '@/api/query-client';
 
 import './styles.css';
 // import reportWebVitals from './reportWebVitals'
@@ -36,10 +32,7 @@ const rootElement = document.getElementById('app')!;
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
 
